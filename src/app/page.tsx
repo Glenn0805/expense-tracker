ba1e9c5fe41ec9554cd903c52e28e2e32f284fb8
tmjp-Glenn <glenn@tmjpbpo.com>
2024-11-28 16:09:03 +0800

    update card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
       <div className="min-h-screen bg-white dark:bg-[#0a0a0a]">
         <Header />
         <div className="p-3">
-          <button className="btn btn-success">Add</button>
+          <button className="btn btn-success mb-2">Add</button>
           <div className="flex flex-row flex-wrap gap-1">
-            <CreditCard currentBalance={2999999099.23} cardColor="red" />
-            <CreditCard currentBalance={2999999099.23} cardColor="green" />
-            <CreditCard currentBalance={2999999099.23} cardColor="blue" />
-            <CreditCard currentBalance={2999999099.23} cardColor="violet" />
-            <CreditCard currentBalance={2999999099.23} cardColor="brown" />
+            <CreditCard currentBalance={2999999099.23} cardColor="red" cardNumber="1231123112311231" />
+            <CreditCard currentBalance={2999999099.23} cardColor="green" cardNumber="3211321132113211" />
+            <CreditCard currentBalance={2999999099.23} cardColor="blue" cardNumber="1111222233334444" />
+            <CreditCard currentBalance={2999999099.23} cardColor="violet" cardNumber="5555666677778888" />
+            <CreditCard currentBalance={2999999099.23} cardColor="brown" cardNumber="4321432143214321" />
           </div>
 
 
